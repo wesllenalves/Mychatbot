ff5c0f99205a401e2d12e0508fd8896ff54baa57
wesllenalves <wesllenalves@gmail.com>
2018-09-21 11:59:26 -0300

    adicionando a funcionalidade de prompt para o usuario poder interagir com o chatbot

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const watson = require('watson-developer-cloud/assistant/v1');
+const prompt = require('prompt-sync')();
 
 const chatbot = new watson({
 	username: '9a9b8afa-fcc5-4f08-80cd-a4b082b16f09',
@@ -20,9 +21,18 @@ function trataResposta(err, resposta){
 		console.log(err); //casso tenha erro
 		return;
 	}
-	//console.log(resposta);
+	//detecta as intenções
+	if (resposta.intents.length > 0) {
+		console.log('Eu detectei a inteção:' + resposta.intents[0].intent)
+	}
 	//exibe a repoata do dialogo caso haja
 	if (resposta.output.text.length >0) {
-		console.log(resposta.output.text);
+		console.log(resposta.output.text[0]);
 	}
+
+	const mensagemUsuario = prompt('>>');
+	chatbot.message({
+		workspace_id,
+		input: {text: mensagemUsuario}
+	},trataResposta);
 }
