15936faf9c643d3b59ddb6697c61fea561cf3994
wesllen1993 <wesllenalves@gmail.com>
2018-09-23 12:43:42 -0300

    adicionando os arquivos

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const watson = require('watson-developer-cloud/assistant/v1');
 const prompt = require('prompt-sync')();
+require('dotenv').config()
 
 const chatbot = new watson({
-	username: '9a9b8afa-fcc5-4f08-80cd-a4b082b16f09',
-	password: 'OOUh2nrwfOvt',
-	version: '2018-09-21',
+	username: process.env.NAME,
+	password: process.env.PASSWORD,
+	version: process.env.VERSION,
 });
 
-const workspace_id = '7c7e32ba-4183-4605-a471-d5d970b47a1b';
+const workspace_id = process.env.WORKSPACE_ID;
 
 //começando a conversação com uma mensagem vazia
 
 chatbot.message({workspace_id}, trataResposta);
 
 
-
+let fimDeConversa = false;
 
 function trataResposta(err, resposta){
 	if (err) {
@@ -23,17 +24,24 @@ function trataResposta(err, resposta){
 	}
 	//detecta as intenções
 	if (resposta.intents.length > 0) {
-		console.log('Eu detectei a inteção:' + resposta.intents[0].intent)
+		console.log('Eu detectei a inteção:' + resposta.intents[0].intent);
+		if(resposta.intents[0].intent == 'despedida'){
+			fimDeConversa = true;
+		}
 	}
 	//exibe a repoata do dialogo caso haja
 	if (resposta.output.text.length >0) {
 		console.log(resposta.output.text[0]);
 	}
 
-	const mensagemUsuario = prompt('>>');
+	if(!fimDeConversa){
+		const mensagemUsuario = prompt('>>');
 	chatbot.message({
 		workspace_id,
 		input: {text: mensagemUsuario},
 		context: resposta.context
 	},trataResposta);
+	}
+
+	
 }
